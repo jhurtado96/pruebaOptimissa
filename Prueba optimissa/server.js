e9bbd89871bd0489006b91d155acd7a0fc02f623
jhurtado96 <javierhurtado98@gmail.com>
2021-04-10 19:13:29 +0200

    correccion mensajes de error

--- Prueba optimissa/server.js
+++ Prueba optimissa/server.js
@@ -1,61 +1,58 @@
 // Creo la clase Cliente con 4 atributos
 
-class Client
-{
+class Client {
 
 
-    constructor (id,name,email,role)
-    {
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.role=role;
+    constructor(id, name, email, role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
     }
 
 
-    
-}
- 
 
- // Creo la clase Policie con 6 atributos
+}
+
+
+// Creo la clase Policie con 6 atributos
 
 class Policie {
 
-    constructor (id, amountInsured,email, inceptionDate, installmentPayment,clientId)
-    {
+    constructor(id, amountInsured, email, inceptionDate, installmentPayment, clientId) {
         this.id = id;
         this.amountInsured = amountInsured;
         this.email = email;
         this.inceptionDate = inceptionDate;
         this.installmentPayment = installmentPayment
-        this.clientId=clientId
-        
+        this.clientId = clientId
+
     }
 
 }
 
 //Creo 4 objetos de la clase cliente y politica donde  Id será la posición del array en el que estará ubicado
 
-let clientOne = new Client ("0","Pablo","pablo@gmail.com","Analista");
-let clientTwo = new Client ("1","luis","luis@gmail.com","dev");
-let clientThree = new Client ("2","jaime","jaime@gmail.com","manager");
-let clientFour = new Client ("3","manuel","manuel@gmail.com","engineer");
+let clientOne = new Client("0", "Pablo", "pablo@gmail.com", "Analista");
+let clientTwo = new Client("1", "luis", "luis@gmail.com", "dev");
+let clientThree = new Client("2", "jaime", "jaime@gmail.com", "manager");
+let clientFour = new Client("3", "manuel", "manuel@gmail.com", "engineer");
 
 
 
-let policieOne = new Policie ("0","3000","po@gmail.com","14/04/1006","3000",clientOne);
-let policieTwo = new Policie ("1","3000","pt@gmail.com","17/02/1206","3000",clientTwo);
-let policieThree = new Policie ("2","3000","pth@gmail.com","17/04/1406","3000",clientThree);
-let policieFour = new Policie ("3","3000","pf@gmail.com","01/01/2006","3000",clientFour);
+let policieOne = new Policie("0", "3000", "po@gmail.com", "14/04/1006", "3000", clientOne);
+let policieTwo = new Policie("1", "3000", "pt@gmail.com", "17/02/1206", "3000", clientTwo);
+let policieThree = new Policie("2", "3000", "pth@gmail.com", "17/04/1406", "3000", clientThree);
+let policieFour = new Policie("3", "3000", "pf@gmail.com", "01/01/2006", "3000", clientFour);
 
 
 // Se crean dos arrays que nos servirán para almacenar los objetos de la clase Politica y Cliente, para así poder acceder a ellos en los end-point
 
 
-let arrPolicies=[policieOne,policieTwo,policieThree,policieFour]
+let arrPolicies = [policieOne, policieTwo, policieThree, policieFour]
 
 
-let arrClients = [clientOne,clientTwo,clientThree,clientFour]
+let arrClients = [clientOne, clientTwo, clientThree, clientFour]
 
 //Utilizo modulo express para el api rest y el jwt para generar el token de autenticacion
 
@@ -63,14 +60,14 @@ const jwt = require("jsonwebtoken");
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-	  //contraseña de JWT que usa para encriptar la información
-      // podría ir en otro fichero pero no es necesario en este caso
+//contraseña de JWT que usa para encriptar la información
+// podría ir en otro fichero pero no es necesario en este caso
 const config = {
-	llave : "miclaveultrasecreta123*"
+    llave: "miclaveultrasecreta123*"
 };
 
 
@@ -78,159 +75,192 @@ app.set('llave', config.llave);
 
 
 //Inicio el servidor en el puerto 3000
-app.listen(3000,()=>{
-    console.log('Servidor iniciado en el puerto 3000') 
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000')
 });
 // Ahora vienen los end point. Prueba unitaria para ver si estoy conectado al servidor
-app.get('/', function (req,res){
+app.get('/', function (req, res) {
     res.send('Hello from server')
 });
 
 // En este end point realizamos el login en el cual hardcodeamos un user y un psw, con el modulo de jwt generamos un token y lo devolvemos al usuario 
 
 app.post('/login', (req, res) => {
-    if(req.body.client_ID === "dare" && req.body.client_secret === "s3cr3t") {
-		const payload = {
-			check:  true
-		};
-		const token = jwt.sign(payload, app.get('llave'), {
-			expiresIn: 1440
-		});
-		res.send({
-            code:200,
-			mensaje: 'Autenticación correcta',
-			token: token,
-            expiresIn:1440
-		});
+    if (req.body.client_ID === "dare" && req.body.client_secret === "s3cr3t") {
+
+        const payload = {
+            check: true
+        };
+
+        const token = jwt.sign(payload, app.get('llave'), {
+            expiresIn: 1440
+        });
+
+        res.send({
+            code: 200,
+            mensaje: 'Autenticación correcta',
+            token: token,
+            expiresIn: 1440
+        });
+
     } else {
-        res.send({code:401, mensaje: "Unauthorized error"})
+
+        res.send({ code: 401, mensaje: "Datos incorrectos" })
+
     }
 })
 
 //En este punto verificamos si el token recibido es válido o no dando feedback al usuario
-const rutasProtegidas = express.Router(); 
+const rutasProtegidas = express.Router();
+
 rutasProtegidas.use((req, res, next) => {
+
     const token = req.headers['access-token'];
-	
+
     if (token) {
-      jwt.verify(token, app.get('llave'), (err, decoded) => {      
-        if (err) {
-            return res.send({code:401, mensaje: 'Unauthorized error' });       
-        } else {
-          req.decoded = decoded;    
-          next();
-        }
-      });
+
+        jwt.verify(token, app.get('llave'), (err, decoded) => {
+
+            if (err) {
+
+                return res.send({ code: 401, mensaje: 'Token no válido' });
+
+            } else {
+
+                req.decoded = decoded;
+                next();
+
+            }
+        });
+
     } else {
-        res.send({ codigo:403, mensaje: 'Forbidden error'});
+
+        res.send({ codigo: 403, mensaje: 'No existe el token' });
+
     }
- });
+});
 
 //End point de clients, llamamos tambien a rutas protegidas para saber si se ha recibido el token correctamente. En este endpoint podemos tanto poner id en la query como si no,
 // si existe el id muestra solo el cliente con ese id, en caso de que no pongamos nada en la query mostrará todos.
 
 app.get("/clients", rutasProtegidas,
-function (req,res) 
-{
-    let respuesta;
-    let id=req.query.id
-    if (arrClients==null) {
-        respuesta={error:false, codigo:201, mensaje:'No hay clientes'}
-    }   
-    
-    if(id)        
-    {
-        if (arrClients.length<id) {
-            respuesta={error:false, codigo:201, mensaje:'No hay clientes con ese id'}
-            
-        } else {
-            respuesta = {error:false, codigo:200 ,mensaje:arrClients[id]};
+
+    function (req, res) {
+
+        let respuesta;
+        let id = req.query.id
+
+        if (arrClients == null) {
+
+            respuesta = { error: false, codigo: 201, mensaje: 'No hay clientes' }
+
         }
-        
-        
-    }else{
-        respuesta={error:false, codigo:200, mensaje:arrClients}
-        
+
+        if (id) {
+            if (arrClients.length < id) {
+
+                respuesta = { error: false, codigo: 201, mensaje: 'No hay clientes con ese id' }
+
+            } else {
+
+                respuesta = { error: false, codigo: 200, mensaje: arrClients[id] };
+
+            }
+
+
+        } else {
+            respuesta = { error: false, codigo: 200, mensaje: arrClients }
+
+        }
+        res.send(respuesta);
+
+
+
     }
-    res.send(respuesta); 
-
-
-    
-} 
 )
 
 //End point de policies, llamamos tambien a rutas protegidas para saber si se ha recibido el token correctamente. En este endpoint podemos tanto poner id en la query como si no,
 // si existe el id muestra solo la politica con ese id junto a su cliente, en caso de que no pongamos nada en la query mostrará todos junto a sus respectivos clientes.
 
 app.get("/policies",
-function (req,res) 
-{
-    let id = req.query.id
-    let respuesta;
-    if (arrPolicies==null) {
-        respuesta={error:false, codigo:199, mensaje:'No hay politicas'}
-    }
-    
-        
-    
-    if(id)        
-    {
-        if (arrPolicies.length<id) {
-            respuesta={error:false, codigo:201, mensaje:'No hay politicas con ese id'}
-        } else {
-            
-            
-            respuesta = {error:false, codigo:200, mensaje:arrPolicies[id]};
+
+    function (req, res) {
+
+        let id = req.query.id
+        let respuesta;
+
+        if (arrPolicies == null) {
+
+            respuesta = { error: false, codigo: 405, mensaje: 'No hay politicas' }
+
         }
-        
-    }else{
-        respuesta={error:false, codigo:200, mensaje:arrPolicies}
-        
+
+
+
+        if (id) {
+            if (arrPolicies.length < id) {
+
+                respuesta = { error: false, codigo: 406, mensaje: 'No hay politicas con ese id' }
+
+            } else {
+
+                respuesta = { error: false, codigo: 200, mensaje: arrPolicies[id] };
+
+            }
+
+        } else {
+            respuesta = { error: false, codigo: 200, mensaje: arrPolicies }
+
+        }
+        res.send(respuesta);
+
+
+
     }
-    res.send(respuesta); 
-
-
-    
-} 
 )
 
 //End point de policies/client, llamamos tambien a rutas protegidas para saber si se ha recibido el token correctamente. En este endpoint tenemos poner id en la query,
 // si no hay id como si no existe mostrará un msje de error ,si existe el id muestra solo el cliente de su respectiva politica.
 
 app.get("/policies/client",
-function (req,res) 
-{
-    let respuesta;
-    
-    
-        
-    let id = req.query.id
-    if(id)        
-    {
-        if (arrPolicies.length<id) {
-            respuesta={error:false, codigo:201, mensaje:'No hay politicas con ese id'}
+
+    function (req, res) {
+
+        let respuesta;
+        let id = req.query.id
+
+        if (id) {
+            if (arrPolicies.length < id) {
+
+                respuesta = { error: false, codigo: 406, mensaje: 'No existe el ID' }
+
+            } else {
+
+                respuesta = { error: false, codigo: 200, mensaje: arrPolicies[id].clientId };
+
+            }
+
         } else {
-            
-            
-            respuesta = {error:false, codigo:200, mensaje:arrPolicies[id].clientId};
+
+            respuesta = { error: false, codigo: 200, mensaje: "Escribe un id" }
+
         }
-        
-    }else{
-        respuesta={error:false, codigo:200, mensaje:"Id inexistente"}
-        
+
+        res.send(respuesta);
+
     }
-    res.send(respuesta); 
-
-
-    
-} 
 )
 
 //En el caso de llamar a un endpoint que no existe mostrará el msj de error 404
 
-app.use(function(req, res, next){
-    respuesta = {codigo: 404, mensaje: "URL no encontrado"}
+app.use(function (req, res, next) {
+
+    respuesta = { codigo: 404, mensaje: "URL no encontrada" }
+
     res.status(404).send(respuesta)
+
 })
 
-module.exports=app
+//Exportamos el modulo app para el testing
+
+module.exports = app
--- Prueba optimissa/tests/api.test.js
+++ Prueba optimissa/tests/api.test.js
@@ -1,20 +1,38 @@
 const app = require('../server')
 const request = require('supertest')
 
-describe("GET /clients",()=>{
-    it('Lista de clientes',done => {
+// Aquí realizo los tests, utilizo el modulo Supertest que provee un nivel alto de abstracción.
+//Importo el modulo app al fichero y llamo los endpoint. El It llama al test, con describe agrupamos los test, seguidamente llamamos al end point correspondiente 
+//en este caso; get /clients, /policies, /policies/client/ y post /login. Seguidamente indicamos que el formato aceptado será el tipo json y si el codigo final es 200 recibiremos
+//el visto bueno del test.
+
+describe("GET /", () => {
+    it('Lista de clientes', done => {
         request(app)
-        .get('/clients')
-        .set('Accept', 'application/json')
-        .expect(200,done)
-    })
-    })
-    
-    describe("GET /policies",()=>{
-        it('Lista de politicas',done => {
-            request(app)
             .get('/clients')
             .set('Accept', 'application/json')
-            .expect(200,done)
-        })
-        })
+            .expect(200, done)
+    })
+
+    it('Lista de politicas', done => {
+        request(app)
+            .get('/policies')
+            .set('Accept', 'application/json')
+            .expect(200, done)
+    })
+
+    it('Cliente de la politica', done => {
+        request(app)
+            .get('/policies/client/')
+            .set('Accept', 'application/json')
+            .expect(200, done)
+    })
+})
+describe("POST /", () => {
+    it('Login', done => {
+        request(app)
+            .post('/login')
+            .set('Accept', 'application/json')
+            .expect(200, done)
+    })
+})
